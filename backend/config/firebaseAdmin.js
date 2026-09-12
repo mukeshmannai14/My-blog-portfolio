65d562b7cde0037848da77f2781bc999db867f2c
mukeshmannai14 <mukeshkannamannai04@gmail.com>
2026-09-12 11:33:12 +0530

    Prepare Firebase Admin for production

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,10 +1,21 @@
-const { initializeApp, cert } = require("firebase-admin/app");
-const { getAuth } = require("firebase-admin/auth");
+const {
+  initializeApp,
+  cert,
+} = require("firebase-admin/app");
 
-const serviceAccount = require("../serviceAccountKey.json");
+const {
+  getAuth,
+} = require("firebase-admin/auth");
 
 const firebaseApp = initializeApp({
-  credential: cert(serviceAccount),
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(
+      /\\n/g,
+      "\n"
+    ),
+  }),
 });
 
 const adminAuth = getAuth(firebaseApp);
